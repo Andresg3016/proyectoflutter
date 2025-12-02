01ca57b1ca934bb64ce792adc26649a77c861936
Andresg3016 <Andresg3016@3016>
2025-12-01 20:42:06 -0500

    Estructura de la app

---
+++ README.md
@@ -0,0 +1,4 @@
+Descripción
+Este proyecto es un ejemplo sencillo de cómo utilizar assets en Flutter, incluyendo la configuración en pubspec.yaml y el uso de imágenes dentro de la aplicación.
+Su propósito es servir como referencia para desarrolladores que necesiten entender cómo cargar y utilizar recursos locales en un proyecto Flutter.
+
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp  extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Proyecto',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Flutter App'),
+        ),
+        body: Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
+
+
