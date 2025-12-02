ed31cdc152503f25eeb0b62ace255223f8a771bf
Andresg3016 <Andresg3016@gmail.com>
2025-12-02 03:03:13 -0500

    Cambio de paginas

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  int _paginaActual = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Proyecto Flutter Scrum 3',
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text('My Flutter App')),
-        body: Center(child: Text('Hello, World!')),
+        appBar: AppBar(title: Text('Market Delivery')),
+        body: Center(
+          child: Container(
+            child: Text(
+              'Bienvenido a Market Delivery',
+              style: TextStyle(fontSize: 25),
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              _paginaActual = index;
+            });
+          },
+          currentIndex: _paginaActual,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.supervised_user_circle),
+              label: "Users",
+            ),
+          ],
+        ),
       ),
     );
-  } // proyecto Scrum 3
+  }
 }
